ca419ec3ef1f746e245b0e99f16f8f9a1fc69709
mengzhao <mengzhao@opay.team>
2021-01-22 13:58:21 +0800

    新增redis判断只启用一台canal 消费者端 升级版本号v1.2.4-SNAPSHOT

--- canal-client/src/main/java/top/javatool/canal/client/client/AbstractCanalClient.java
+++ canal-client/src/main/java/top/javatool/canal/client/client/AbstractCanalClient.java
@@ -2,13 +2,15 @@ package top.javatool.canal.client.client;
 
 import com.alibaba.otter.canal.client.CanalConnector;
 import com.alibaba.otter.canal.protocol.Message;
-
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 import org.apache.commons.lang.StringUtils;
+import org.redisson.api.RBucket;
+import org.redisson.api.RedissonClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import top.javatool.canal.client.handler.MessageHandler;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * @author yang peng
@@ -18,94 +20,110 @@ import java.util.concurrent.TimeUnit;
 public abstract class AbstractCanalClient implements CanalClient {
 
 
-    protected volatile boolean flag;
+  protected volatile boolean flag;
 
 
-    private Logger log = LoggerFactory.getLogger(AbstractCanalClient.class);
+  private Logger log = LoggerFactory.getLogger(AbstractCanalClient.class);
 
 
-    private Thread workThread;
+  private Thread workThread;
 
 
-    private CanalConnector connector;
+  private CanalConnector connector;
 
 
-    protected String filter = StringUtils.EMPTY;
+  protected String filter = StringUtils.EMPTY;
+
+  private static final String LOCK_NAME = "spark:api:canal:start";
 
 
-    protected Integer batchSize = 1;
+  protected Integer batchSize = 1;
 
 
-    protected Long timeout = 1L;
+  protected Long timeout = 1L;
 
 
-    protected TimeUnit unit = TimeUnit.SECONDS;
+  protected TimeUnit unit = TimeUnit.SECONDS;
 
 
+  private MessageHandler messageHandler;
 
-    private MessageHandler messageHandler;
+  @Autowired
+  RedissonClient redissonClient;
 
+  private static final String CURRENT_APP_ID = UUID.randomUUID().toString();
 
+  @Override
+  public void start() {
+    log.info("start canal client");
+    RBucket<String> bucket = redissonClient.getBucket(LOCK_NAME);
+    if (bucket.isExists() && !bucket.get().equals(CURRENT_APP_ID)) {
+      log.info("\n\n\n\ncanal client is runing anohter app now\n\n\n\n");
+      return;
+    }
+    bucket.set(CURRENT_APP_ID, 50 * 10, TimeUnit.SECONDS);
+    workThread = new Thread(this::process);
+    workThread.setName("canal-client-thread");
+    flag = true;
+    workThread.start();
+    log.info("canal client started...........");
+  }
 
-    @Override
-    public void start() {
-        log.info("start canal client");
-        workThread = new Thread(this::process);
-        workThread.setName("canal-client-thread");
-        flag = true;
-        workThread.start();
+  @Override
+  public void stop() {
+    log.info("stop canal client");
+    RBucket<String> bucket = redissonClient.getBucket(LOCK_NAME);
+    if (bucket.isExists() && bucket.get().equals(CURRENT_APP_ID)) {
+      log.info("releasing canal client bucket ...........");
+      bucket.deleteAsync();
+    }
+    flag = false;
+    if (null != workThread) {
+      workThread.interrupt();
     }
 
-    @Override
-    public void stop() {
-        log.info("stop canal client");
-        flag = false;
-        if (null != workThread) {
-            workThread.interrupt();
-        }
+  }
 
-    }
-
-    @Override
-    public void process() {
+  @Override
+  public void process() {
+    while (flag) {
+      try {
+        connector.connect();
+        connector.subscribe(filter);
         while (flag) {
-            try {
-                connector.connect();
-                connector.subscribe(filter);
-                while (flag) {
-                    Message message = connector.getWithoutAck(batchSize, timeout, unit);
+          Message message = connector.getWithoutAck(batchSize, timeout, unit);
 //                    log.info("获取消息 {}", message);
-                    long batchId = message.getId();
-                    if (message.getId() != -1 && message.getEntries().size() != 0) {
-                        messageHandler.handleMessage(message);
-                    }
-                    connector.ack(batchId);
-                }
-            } catch (Exception e) {
-                log.error("canal client 异常", e);
-            } finally {
-                connector.disconnect();
-            }
+          long batchId = message.getId();
+          if (message.getId() != -1 && message.getEntries().size() != 0) {
+            messageHandler.handleMessage(message);
+          }
+          connector.ack(batchId);
         }
+      } catch (Exception e) {
+        log.error("canal client 异常", e);
+      } finally {
+        connector.disconnect();
+      }
     }
+  }
 
 
-    public void setConnector(CanalConnector connector) {
-        this.connector = connector;
-    }
+  public void setConnector(CanalConnector connector) {
+    this.connector = connector;
+  }
 
 
-    public void setMessageHandler(MessageHandler messageHandler) {
-        this.messageHandler = messageHandler;
-    }
+  public void setMessageHandler(MessageHandler messageHandler) {
+    this.messageHandler = messageHandler;
+  }
 
 
-    public CanalConnector getConnector() {
-        return connector;
-    }
+  public CanalConnector getConnector() {
+    return connector;
+  }
 
 
-    public MessageHandler getMessageHandler() {
-        return messageHandler;
-    }
+  public MessageHandler getMessageHandler() {
+    return messageHandler;
+  }
 }
